df175dc24c04c48429fa5655d8e512da1e6b2675
chrise <leetesla@sina.com>
2026-06-22 09:47:43 +0800

    M

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,6 @@
 import { defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
-
 const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
